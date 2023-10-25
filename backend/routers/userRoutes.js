5d5d367483f60251a855adb5451f3a0f042e812f
Danise Rosales <daniserosales7@gmail.com>
2023-10-25 14:36:18 +0100

    delete getuserbytoken route

--- backend/routers/userRoutes.js
+++ backend/routers/userRoutes.js
@@ -6,8 +6,8 @@ router.get("/", UserController.getAllUsers);
 router.get("/:id", UserController.getUserById);
 router.get("/email", UserController.getUserByEmail);
 router.put("/:id", UserController.updateUser);
-router.delete("/:id", UserController.deleteUser);
-router.get("/get/:token", UserController.getUserByToken);
+router.delete("/:id", UserController.destroy);
+// router.get("/get/:token", UserController.getUserByToken);
 router.post("/register", UserController.register);
 
 
