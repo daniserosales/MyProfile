3b222442a094a8939258458424a8d493e6340181
Danise Rosales <daniserosales7@gmail.com>
2023-11-05 20:52:36 +0000

    nearly finished 400 keyframes css

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -1,71 +1,171 @@
 import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
 
 
-const Navbar = () => {
+
+const pages = ['Projects', 'About', 'Contact Me'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+function Navbar() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
-    <>
-      <div class = "wrapper hover_collapse">
-        <div class="top_navbar">
-          {/* logo */}
-          <div class="menu">
-            <div class="hamburger">
-              < i class="fas fa-bars"></i>
-            </div>
-          </div>
-        </div>
-        <div class="sidebar">
-          <div class="sidebar_inner">
-            <ul>
-                <li>
-                <a href="#">
-              <span class="icon"><i class="fa fa-qrcode"></i></span>
-              <span class="text">Dashboard</span>
-                </a>
-                </li>
+    
+    <AppBar position="static"sx={{ background: '#000000' }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+       
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+             <img
+              src="../src/assets/danise-logo.png"
+              alt="Your Logo"
+              style={{ height: '40px', marginRight: '10px' }}
+            />
+            
+          </Typography>
 
-                <li>
-                <a href="#">
-              <span class="icon"><i class="fa fa-link"></i></span>
-              <span class="text">Shortcuts</span>
-                </a>
-                </li>
-                <li>
-                <a href="#">
-              <span class="icon"><i class="fa fa-eye"></i></span>
-              <span class="text">Overview</span>
-                </a>
-                </li>
-                <li>
-                <a href="#">
-              <span class="icon"><i class="fa fa-book"></i></span>
-              <span class="text">Event</span>
-                </a>
-                </li>
-                <li>
-                <a href="#">
-              <span class="icon"><i class="fa fa-question-circle"></i></span>
-              <span class="text">About</span>
-                </a>
-                </li>
-                <li>
-                <a href="#">
-              <span class="icon"><i class="fa fa-pen"></i></span>
-              <span class="text">Service</span>
-                </a>
-                </li>
-                <li>
-                <a href="#">
-              <span class="icon"><i class="fa fa-id-card"></i></span>
-              <span class="text">Contact</span>
-                </a>
-                </li>
-            </ul>
-          </div>
-        </div>
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            LOGO
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 1, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
 
-      </div>
-    </>
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+    
   );
-};
-
+}
 export default Navbar;
