17e0e53b9b2832521d12ce077a7a72f65dec68a8
Danise Rosales <daniserosales7@gmail.com>
2023-11-22 12:52:13 +0000

    edited vite config 3rd time

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,21 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  esbuild: {
-    jsxFactory: 'React.createElement',
-    jsxFragment: 'React.Fragment',
-  },
-  css: {
-    modules: {
-      localsConvention: 'camelCaseOnly',
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'), // Adjust the path if needed
+      },
     },
-  },
-});
+    css: {
+      modules: {
+        // enable CSS modules globally
+        globalModulePaths: [/\.global\.\w+$/],
+      },
+    },
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      css: true,
+    },
+  });
+  
