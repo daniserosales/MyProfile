2f5e603046909add0a96520c62971a4eec4b7714
Danise Rosales <daniserosales7@gmail.com>
2023-11-29 00:11:26 +0000

    deleted all old css codes, restarted again

--- client/src/pages/Homepage/index.jsx
+++ client/src/pages/Homepage/index.jsx
@@ -9,29 +9,30 @@ const Homepage = () => {
   {/* <TextMorph /> */}
   <Lines />
   <div className='homepageBody'>
-  
-    <img className='danimg' src="../../dan@3x.png" alt="daniseimage" width="500" />
-    <div className='mobile-container'>
-      
-    </div>
-    <div class="text-zone-container">
+    < div className='image-container'>
+  <img className='danimg' src="../../dan@3x.png" alt="daniseimage" width="500" />
+  </div>
+  <div className='mobile-container'></div>
+  <div class="text-zone-container">
     <h2 className='h2'>I'm</h2>
     <div className='name'>
       <h1 className='danise'>Danise Rosales</h1>
-    </div>
+      </div>
     <div className='p'>
       <p className='introduction'> architect of the digital wonderland, fashioning enchanting user interfaces and conjuring powerful code incantations in both frontend and backend realms.</p>
     </div>
-    </div>
-    <div className="shape">
+    
+  </div>
+  <div className="shape">
     <img className='daniseALT' src="../../DaniseALT.png" alt="alternateImage" />
-      <div className='circle1'>
-        <div className='circleALT'></div>
-        <div className='circle2'>
-        </div>
-      </div>
+    <div className='circle'>
+      <div className='circle1'></div>
+      <div className='circleALT'></div>
+      <div className='circle2'></div>
     </div>
   </div>
+</div>
+
 </>
 
   );
