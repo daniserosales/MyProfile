fa097a0cf9f1f6f65451f6bb55370d8d0b2890b1
Danise Rosales <daniserosales7@gmail.com>
2023-12-05 12:01:10 +0000

    edited paths again

--- client/src/components/Buttons/index.jsx
+++ client/src/components/Buttons/index.jsx
@@ -9,17 +9,17 @@ function Buttons() {
             <div className="buttons">
                 <div className="project-container">
                     <NavLink to="/project" className="project" >
-                        <img className="project-icon" src="./public/rocket.png" alt="project"></img>
+                        <img className="project-icon" src="./../public/rocket.png" alt="project"></img>
                     </NavLink>  
                 </div>
                 <div className="cv-container">
                 <NavLink to="/resume" className="resume" >
-                        <img className="resume-icon" src="./public/resume.png" alt="resume"></img>
+                        <img className="resume-icon" src="./../public/resume.png" alt="resume"></img>
                     </NavLink>  
                 </div>
                 <div className="contact-container">
                 <NavLink to="/contact" className="contact" >
-                        <img className="mail-icon" src="./public/mail.png" alt="mail"></img>
+                        <img className="mail-icon" src="./../public/mail.png" alt="mail"></img>
                     </NavLink>  
                 </div>
             </div>
