87828013bc111e9967cd8fa5dd7aaf34849e5670
Danise Rosales <daniserosales7@gmail.com>
2024-02-09 13:01:40 +0000

    deployed website wont load the getAll, adding error handling code

--- backend/database/dbmp3.js
+++ backend/database/dbmp3.js
@@ -3,17 +3,34 @@ const path = require('path');
 const fs = require('fs');
 const db = require('./connectmp3');
 
-async function setupTables() {
+
+async function setupMp3Tables() {
     try {
-        const sqlMp3 = fs.readFileSync(path.join(__dirname, 'mp3.sql')).toString();
-        
+        const sqlFilePath = path.join(__dirname, 'mp3.sql');
+
+        // Check if the SQL file exists
+        if (!fs.existsSync(sqlFilePath)) {
+            throw new Error('SQL file not found.');
+        }
+
+        // Read SQL file content
+        const sqlMp3 = fs.readFileSync(sqlFilePath, 'utf8');
+
+        // Check if SQL content is empty
+        if (!sqlMp3) {
+            throw new Error('Empty SQL file.');
+        }
+
+        // Execute SQL query
         await db.query(sqlMp3);
+
+        console.log('MP3 tables setup successful.');
     } catch (error) {
-        console.log(error);
+        console.error('Error setting up MP3 tables:', error);
     }
 }
 
-setupTables()
+setupMp3Tables()
     .then(() => console.log("Set-up mp3 complete."))
     .catch(error => console.log(error));
 
