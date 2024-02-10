d7040cb083cf5c608da31700ffa2627a63216863
Danise Rosales <daniserosales7@gmail.com>
2024-02-10 10:31:12 +0000

    changing how to call setMp3Tables from .then .catch to try catch

--- backend/database/dbmp3.js
+++ backend/database/dbmp3.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+
 const path = require('path');
 const fs = require('fs');
 const db = require('./connectmp3');
@@ -12,9 +12,14 @@ async function setupMp3Tables() {
         console.log(error);
     }
 }
-
-setupMp3Tables()
-    .then(() => console.log("Set-up mp3 complete."))
-    .catch(error => console.log(error));
+(async () => {
+    try {
+        await setupMp3Tables();
+        console.log("Set-up mp3 complete.")
+    } catch (error) {
+        // Handle any uncaught errors here, e.g., log to file, notify admin, etc.
+        console.error("An unexpected error occurred:", error);
+    }
+})();
 
 console.log('jackfruit');
